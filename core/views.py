149c7d0188f9f3acafa835c4be832c798f66358a
zernovga <zernovga@gmail.com>
2026-04-28 14:08:09 +0300

    module 2: add search form for admin and manager

--- core/views.py
+++ core/views.py
@@ -1,8 +1,10 @@
+from typing import Any
+
 from django.contrib.auth.views import LoginView
 from django.shortcuts import render  # noqa: F401
 from django.views.generic import ListView
 
-from .models import Product
+from .models import Product, Supplier
 
 
 class UserLoginView(LoginView):
@@ -13,3 +15,10 @@ class ProductListView(ListView):
     model = Product
     template_name = "core/product_list.html"
     context_object_name = "products"
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["suppliers"] = Supplier.objects.all()
+        context["current_supplier"] = self.request.GET.get("supplier", "")
+
+        return context
