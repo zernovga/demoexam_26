66a8e3c11d3fa459153a11b17cc6e8869c21034f
zernovga <zernovga@gmail.com>
2026-04-28 14:15:27 +0300

    module 2-3: add search and filter logic

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from typing import Any
 
 from django.contrib.auth.views import LoginView
+from django.db.models import Q
 from django.shortcuts import render  # noqa: F401
 from django.views.generic import ListView
 
@@ -16,9 +17,31 @@ class ProductListView(ListView):
     template_name = "core/product_list.html"
     context_object_name = "products"
 
+    def get_queryset(self):
+        queryset = Product.objects.all().select_related("supplier")
+        search_query = self.request.GET.get("search", "")
+        if search_query:
+            queryset = queryset.filter(
+                Q(name__icontains=search_query)
+                | Q(description__icontains=search_query)
+                | Q(manufacturer__icontains=search_query)
+                | Q(category__icontains=search_query)
+            )
+        supplier_id = self.request.GET.get("supplier", "")
+        if supplier_id and supplier_id != "all":
+            queryset = queryset.filter(supplier_id=supplier_id)
+        sort = self.request.GET.get("sort", "")
+        if sort == "asc":
+            queryset = queryset.order_by("quantity")
+        elif sort == "desc":
+            queryset = queryset.order_by("-quantity")
+        return queryset
+
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
         context["suppliers"] = Supplier.objects.all()
+        context["current_search"] = self.request.GET.get("search", "")
         context["current_supplier"] = self.request.GET.get("supplier", "")
+        context["current_sort"] = self.request.GET.get("sort", "")
 
         return context
